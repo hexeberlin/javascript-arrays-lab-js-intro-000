c3afd31736f9d01f7ed4b5c4025c1e195345123f
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-11-01 13:04:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  return kittens.splice(0, kittens.length-1)
+  return kittens.slice(0, kittens.length-1)
 }
 
 function removeFirstKitten(){
-  return kittens.splice(1, kittens.length)
+  return kittens.slice(1, kittens.length)
 }
